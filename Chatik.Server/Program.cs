981c505d7cbf00865a9b64b8badafc21268ba0c4
DimucaTheDev <74795935+DimucaTheDev@users.noreply.github.com>
2023-11-28 23:10:25 +0300

    Добавьте файлы проекта.

---
+++ Chatik.Server/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Text.Json;
+using SuperSimpleTcp;
+
+namespace Chatik.Server
+{
+    public record Message(string date, string user, string text);
+    internal class Program
+    {
+        public static List<Message> history;
+        public const int MAX_HISTORY_SIZE = 50;
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> ipName = new();
+            history = new();
+            SimpleTcpServer s = new("0.0.0.0:286");
+            s.Start();
+            Console.WriteLine("started");
+            void AddHistory(Message msg)
+            {
+                history.Insert(0, msg);
+                if(history.Count >= MAX_HISTORY_SIZE)
+                history.RemoveRange(MAX_HISTORY_SIZE-1, 1);
+            }
+            void SendAll(string name, string content)
+            {
+                try
+                {
+                    AddHistory(new(DateTime.Now.ToString("HH:mm:ss"), name, content));
+                    s.GetClients().ToList()
+                        .ForEach(ip => s.Send(ip, $"{{\"user\":\"{name}\",\"text\":\"{content}\"}}"));
+                }
+                catch { }
+            }
+            s.Events.ClientDisconnected += (ss, e) =>
+            {
+                if (s.Connections != 0)
+                    SendAll("System", $"User {ipName[e.IpPort]} disconnected");
+                Console.WriteLine($"-{ipName[e.IpPort]}");
+                ipName.Remove(e.IpPort);
+            };
+            s.Events.DataReceived += (ss, e) =>
+            {
+                string content = System.Text.Encoding.Default.GetString(e.Data!);
+                if (content.Length == 0) return;
+                if (content[0] == '+')
+                {
+                    ipName.Add(e.IpPort, content.Remove(0, 1));
+                    SendAll("System", $"User {content.Remove(0, 1)} joined the chat.");
+                }
+                else if (content[0] == 'H')
+                {
+                    history.Reverse();
+                    s.Send(e.IpPort, "H" + Newtonsoft.Json.JsonConvert.SerializeObject(history));
+                    history.Reverse();
+                }
+                else SendAll(ipName[e.IpPort], content.Remove(0, 1));
+                Console.WriteLine(content);
+            };
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    if (s.Connections == 0) continue;
+                    var sysMsg = Console.ReadLine();
+                    SendAll("System", sysMsg);
+                }
+            }).Start();
+        }
+    }
+}
---
+++ Chatik.TestClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Linq;
+using System;
+using System.Text;
+using System.Threading.Channels;
+using Newtonsoft.Json;
+using SuperSimpleTcp;
+
+namespace Chatik.TestClient
+{
+    internal class Chat
+    {
+        public static SimpleTcpClient tcp;
+        static void Main(string[] args)
+        {
+            Chat.tcp = new("127.0.0.1:286");
+            Chat.tcp.Events.Connected += (ss, ee) => Console.WriteLine("Connected");
+            Chat.tcp.Events.Disconnected += (ss, ee) => Console.WriteLine("Disconnected");
+            Chat.tcp.Connect();
+            Chat.tcp.Send($"+TestClient");
+            tcp.Events.DataReceived += (s, e) =>
+            {
+                var ssss = Encoding.UTF8.GetString(e.Data);
+                dynamic json = JsonConvert.DeserializeObject(ssss);
+                string message = $"[{DateTime.Now.ToString("HH:mm:ss")}] {json.user} > {json.text}";
+                Console.WriteLine(message);
+            };
+            while (true)
+            {
+                var msg = Console.ReadLine();
+                tcp.Send($"M{msg}");
+            }
+        }
+    }
+}
